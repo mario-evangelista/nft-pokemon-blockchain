942c9233d6a4293bcacc37af4ef2337c6c23e4d6
Caio Melo <caio.videmelo@gmail.com>
2024-07-25 16:44:12 -0300

    Update PokeDIO.sol
    Add EvolutionState in the struct, create function evolvePokemon, updating the battle function and updating the pragma solidity version to 0.8.24

--- PokeDIO.sol
+++ PokeDIO.sol
@@ -1,14 +1,17 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract PokeDIO is ERC721 {
+    enum EvolutionState { None, Evolved }
+
     struct Pokemon {
         string name;
         uint level;
         string img;
+        EvolutionState evolutionState;
     }
 
     Pokemon[] public pokemons;
@@ -26,6 +29,34 @@ contract PokeDIO is ERC721 {
         _;
     }
 
+    function createNewPokemon(
+        string memory _name,
+        address _to,
+        string memory _img
+    ) public {
+        require(
+            msg.sender == gameOwner,
+            "Apenas o dono do jogo pode criar novos Pokemons"
+        );
+        uint id = pokemons.length;
+        pokemons.push(Pokemon(_name, 1, _img, EvolutionState.None));
+        _safeMint(_to, id);
+    }
+
+    function evolvePokemon(uint _pokemonId) public onlyOwnerOf(_pokemonId) {
+        Pokemon storage pokemon = pokemons[_pokemonId];
+        
+        require(pokemon.level >= 10, "O Pokemon deve estar no nível 10 ou superior para evoluir.");
+        
+        // Atualiza o estado de evolução
+        pokemon.evolutionState = EvolutionState.Evolved;
+
+        // Aqui você pode definir a nova forma do Pokémon, se necessário
+        // Exemplo de mudança de nome ou imagem após a evolução
+        // pokemon.name = "Novo Nome"; // Defina o novo nome
+        // pokemon.img = "Nova Imagem"; // Defina a nova imagem
+    }
+
     function battle(
         uint _attackingPokemon,
         uint _defendingPokemon
@@ -40,19 +71,10 @@ contract PokeDIO is ERC721 {
             attacker.level += 1;
             defender.level += 2;
         }
-    }
 
-    function createNewPokemon(
-        string memory _name,
-        address _to,
-        string memory _img
-    ) public {
-        require(
-            msg.sender == gameOwner,
-            "Apenas o dono do jogo pode criar novos Pokemons"
-        );
-        uint id = pokemons.length;
-        pokemons.push(Pokemon(_name, 1, _img));
-        _safeMint(_to, id);
+        // Verifica se o Pokémon atacante pode evoluir
+        if (attacker.level >= 10 && attacker.evolutionState == EvolutionState.None) {
+            evolvePokemon(_attackingPokemon);
+        }
     }
 }
